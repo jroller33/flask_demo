a5e83d4bad9098cd2525d828b16779f96ff716c5
John Roller <jtroller33@gmail.com>
2023-02-01 20:32:49 -0600

    debugging

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-import re
+import re   # regex used for filtering user input
 from datetime import datetime
 
 from flask import Flask
-app = Flask(__name__)
+
+app = Flask(__name__)   # creates an instance of FLask
     
 # press Ctrl-shift-p and search python interpreter. then paste the path of 'python.exe' example: (J:\GH Repos\flask_demo\.venv\Scripts\python.exe) paste this into where it asks for the path of the interpreter. this uses the interpreter installed inside the virtual environment. DON'T USE THE SYSTEM PYTHON INTERPRETER! KEEP THE VENV CLEAN
 
@@ -12,7 +13,7 @@ def home():
 
 
 @app.route("/hello/<name>")
-# <name> is a variable in the URL that's passed to the fn and can be used in the code
+# <name> is a variable in the URL that's passed to the fn `hello_there(name)` and can be used in the code
 # http://127.0.0.1:5000/hello/john returns "Hello there john! It's Wednesday, 01 February, 2023 at 20:18:53"
 
 
